de5ce9ffaa9e3509105a5582edbc972e3ac1cd91
traveleroindia <traveleroindia@gmail.com>
2025-04-14 17:40:29 +0530

    Sharp Installed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    images: {
+      unoptimized: true, // Disables sharp-based optimization
+    },
+  };
+
 export default nextConfig;
