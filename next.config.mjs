cfc63d53a54a2b417440e6ecc08f3ea632a0c0b1
traveleroindia <traveleroindia@gmail.com>
2025-04-25 11:31:53 +0530

    Revert "Sharp Installed"
    This reverts commit de5ce9ffaa9e3509105a5582edbc972e3ac1cd91.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    images: {
-      unoptimized: true, // Disables sharp-based optimization
-    },
-  };
-
 export default nextConfig;
